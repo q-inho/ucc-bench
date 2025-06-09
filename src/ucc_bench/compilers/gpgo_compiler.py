372300047f6be270470922f93f4b3a97ba66e584
q-inho <f0402043@gmail.com>
2025-06-09 22:25:48 +0900

    benchmark test

---
+++ src/ucc_bench/compilers/gpgo_compiler.py
@@ -0,0 +1,31 @@
+from .base_compiler import BaseCompiler
+from ucc import __version__ as ucc_version
+from ucc import compile
+from ucc.transpilers import CommutationAwarePeephole
+
+# UCC uses qiskit internally
+## TODO: ucc should expose a circuit type vs. assuming always qiskit?
+from qiskit import QuantumCircuit
+from qbraid import transpile
+from ..registry import register
+
+
+@register.compiler("peephole")
+class UCCCompiler(BaseCompiler[QuantumCircuit]):
+    """
+    Wrapper for benchmarking ucc compiler.
+    """
+
+    @classmethod
+    def version(cls) -> str:
+        return ucc_version
+
+    def qasm_to_native(self, qasm: str) -> QuantumCircuit:
+        # Need to manually specifiy since id != "qiskit"
+        return transpile(qasm, "qiskit")
+
+    def compile(self, circuit: QuantumCircuit, ) -> QuantumCircuit:
+        return compile(circuit, target_gateset={"rx", "ry", "rz", "h", "cx"}, enable_peephole=True)
+
+    def count_multi_qubit_gates(self, circuit: QuantumCircuit) -> int:
+        return circuit.num_nonlocal_gates()
