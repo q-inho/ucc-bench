fce6ecff4672c5641d4f76a1fa8cef36b4cc1e8d
q-inho <f0402043@gmail.com>
2025-06-12 09:10:29 +0900

    add dustra compiler

---
+++ src/ucc_bench/compilers/duostra_compiler.py
@@ -0,0 +1,42 @@
+from .base_compiler import BaseCompiler
+from ucc import __version__ as ucc_version
+from ucc import compile
+from ucc.transpilers.duostra_pass import DuostraPass
+
+# UCC uses qiskit internally
+## TODO: ucc should expose a circuit type vs. assuming always qiskit?
+from qiskit import QuantumCircuit
+from qiskit.transpiler import Target, CouplingMap
+from typing import Optional
+from qbraid import transpile
+from ..registry import register
+
+
+
+
+@register.compiler("duostra")
+class UCCCompiler(BaseCompiler[QuantumCircuit]):
+    """
+    Wrapper for benchmarking ucc compiler.
+    """
+
+    @classmethod
+    def version(cls) -> str:
+        return ucc_version
+
+    def qasm_to_native(self, qasm: str) -> QuantumCircuit:
+        # Need to manually specifiy since id != "qiskit"
+        return transpile(qasm, "qiskit")
+
+    def compile(
+        self, circuit: QuantumCircuit, target_device: Optional[Target] = None
+    ) -> QuantumCircuit:
+        return compile(
+            circuit,
+            target_gateset={"rx", "ry", "rz", "h", "cx"},
+            target_device=target_device,
+            custom_passes=[DuostraPass(target_device, strategy="LE", lookahead_depth=3)],
+        )
+
+    def count_multi_qubit_gates(self, circuit: QuantumCircuit) -> int:
+        return circuit.num_nonlocal_gates()
